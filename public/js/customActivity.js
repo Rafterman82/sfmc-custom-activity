0db2d49fe2c8c93b87ea6d617628b6d978dbc43f
Paul Rafter <paulrafter@hotmail.com>
2019-10-29 20:56:18 +0000

    Update public/js/customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -273,17 +273,20 @@ define([
             prePop = 'online';
             steps[1].active = true;
             steps[3].active = true;
+            connection.trigger('updateSteps', steps);
             showStep(null, 3);
         } else if ( !mcOnlineBool && mcInstoreBool ) {
             prePop = 'instore';
             steps[2].active = true;
             steps[3].active = true;
+            connection.trigger('updateSteps', steps);
             showStep(null, 3);
         } else  if ( mcOnlineBool && mcInstoreBool ) {
             prePop = 'online_instore';
             steps[1].active = true;
             steps[2].active = true;
             steps[3].active = true;
+            connection.trigger('updateSteps', steps);
             showStep(null, 3);
         } else{
             prePop = 'not-set';
