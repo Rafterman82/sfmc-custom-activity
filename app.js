116144caeec9a2186338d59fcc9a9e433ca96ff0
Paul Rafter <paulrafter@hotmail.com>
2019-11-25 21:11:51 +0000

    Update app.js

--- app.js
+++ app.js
@@ -412,7 +412,7 @@ app.post('/dataextension/add', urlencodedparser, function (req, res){
 
    	axios.get("https://mc-jb-custom-activity-ca.herokuapp.com/dataextension/lookup/increments").then(response => {
         // If request is good...
-        console.dir(response.items);
+        console.dir(response.data);
         //res.json(response.data.items.values);
         res.json({"success": true});
 
