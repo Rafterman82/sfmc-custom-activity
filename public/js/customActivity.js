4958716cdbf5c0452338d90a3a0f16620f5bdef4
Paul Rafter <paulrafter@hotmail.com>
2019-11-26 15:29:49 +0000

    Update public/js/customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -1035,6 +1035,10 @@ define([
 
         payload['metaData'].isConfigured = true;
 
+        if ( debug ) {
+            console.log(payload);
+        }
+
         connection.trigger('updateActivity', payload);
     }
 
