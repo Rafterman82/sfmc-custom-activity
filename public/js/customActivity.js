818cc36db93037f88a49e4600e8908ac8f36618f
Paul Rafter <paulrafter@hotmail.com>
2020-02-24 14:41:41 +0000

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -250,7 +250,7 @@ define([
                 $("#" + endpointSelector + " > div > div").addClass("slds-theme_success");
                 $("#" + endpointSelector + " > div > div > span:nth-child(2)").addClass("slds-icon-utility-success");
                 $("#" + endpointSelector + " > div > div > span:nth-child(2) svg use").attr("xlink:href","/assets/icons/utility-sprite/svg/symbols.svg#success");
-                $("#" + endpointSelector + " > div > div > div:nth-child(2) h2").text($("#" + endpointSelector + " > div > div > div:nth-child(2) h2").text().replace("Loading", "Successfully Loaded"));
+                $("#" + endpointSelector + " > div > div > .slds-notify__content h2").text($("#" + endpointSelector + " > div > div > .slds-notify__content h2").text().replace("Loading", "Successfully Loaded"));
             }, apiWaitTime);
         
         } else {
@@ -260,7 +260,7 @@ define([
                 $("#" + endpointSelector + " > div > div").addClass("slds-theme_error");
                 $("#" + endpointSelector + " > div > div > span:nth-child(2)").addClass("slds-icon-utility-error");
                 $("#" + endpointSelector + " > div > div > span:nth-child(2) svg use").attr("xlink:href","/assets/icons/utility-sprite/svg/symbols.svg#error");
-                $("#" + endpointSelector + " > div > div > div:nth-child(2) h2").text($("#" + endpointSelector + " > div > div > div:nth-child(2) h2").text().replace("Loading", "Error Loading"));
+                $("#" + endpointSelector + " > div > div > .slds-notify__content h2").text($("#" + endpointSelector + " > div > div > .slds-notify__content h2").text().replace("Loading", "Error Loading"));
             }, apiWaitTime);
         }
 
