a9789b7a2c612dcf1d77394efed28735b52079d2
Paul Rafter <paulrafter@hotmail.com>
2020-03-05 09:07:20 +0000

    Update app.js

--- app.js
+++ app.js
@@ -335,8 +335,8 @@ app.get("/dataextension/lookup/templates", (req, res, next) => {
 // insert data into data extension
 app.post('/dataextension/add', function (req, res){ 
 	
-	console.dir(JSON.stringfy(req.body));
-	res.send(JSON.stringfy(req.body));
+	console.dir(JSON.stringify(req.body));
+	res.send(JSON.stringify(req.body));
 
 });
 
