c939d5203320df8814da79854d1555b4f2fcb0fa
Paul Rafter <paulrafter@hotmail.com>
2020-03-06 15:56:13 +0000

    Update app.js

--- app.js
+++ app.js
@@ -352,7 +352,7 @@ app.post('/dataextension/add', function (req, res){
         "keys": {
             "promotion_key": 12345
         },
-        "values": JSON.stringify(campaignPromotionAssociationData)
+        "values": {campaignPromotionAssociationData}
     }];
 
     console.dir(associationPayload);
